dc69e4fdba6cfe916888b27260a739a8ff1c5a29
Alejandro Castellanos <desarrollo@servitrans.local>
2018-04-09 13:21:01 -0500

    proyecto mvc tienda dedicada a la venta de productos publicitarios

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Administracion()
+        {
+            ViewBag.Message = "Administracion de permisos";
+
+            return View();
+        }
+
+        public ActionResult Registrar_material()
+        {
+            ViewBag.Message = "Registar material";
+
+            return View();
+        }
+
+        public ActionResult Ventas()
+        {
+            ViewBag.Message = "Ventas";
+
+            return View();
+        }
+
+        public ActionResult Ordenes_venta()
+        {
+            ViewBag.Message = "Ordenes de venta";
+
+            return View();
+        }
+    }
+}
